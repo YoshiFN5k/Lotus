e9b93be4c7251f32c10288a441798f424e441182
YoshiFN5k <142546547+YoshiFN5k@users.noreply.github.com>
2023-09-21 18:01:22 +0100

    Update Wraith.cs

--- src/Roles/RoleGroups/Impostors/Wraith.cs
+++ src/Roles/RoleGroups/Impostors/Wraith.cs
@@ -1,119 +1,64 @@
-using System.Collections.Generic;
-using System.Linq;
+using AmongUs.GameOptions;
 using Lotus.API.Odyssey;
-using Lotus.API.Player;
 using Lotus.Factions;
 using Lotus.GUI;
 using Lotus.GUI.Name;
-using Lotus.Options;
+using Lotus.Managers.History.Events;
+using Lotus.Roles.Interactions;
+using Lotus.Roles.Interactions.Interfaces;
+using Lotus.Roles.Internals;
 using Lotus.Roles.Internals.Attributes;
-using Lotus.Roles.Overrides;
 using Lotus.Roles.RoleGroups.Vanilla;
+using Lotus.Utilities;
+using Lotus.API;
+using Lotus.Extensions;
+using Lotus.Options;
 using UnityEngine;
 using VentLib.Options.Game;
-using VentLib.Options.IO;
 using VentLib.Utilities;
+using Lotus.Roles.Interfaces;
+using Lotus.Roles.Overrides;
+using VentLib.Localization.Attributes;
 using VentLib.Utilities.Collections;
-using VentLib.Utilities.Extensions;
 
 namespace Lotus.Roles.RoleGroups.Impostors;
 
-public class BountyHunter: Impostor
+public class Wraith : Impostor
 {
-    private Cooldown acquireNewTarget = null!;
-    private FrozenPlayer? bountyTarget;
+    private Cooldown dashCooldown;
+    private Cooldown dashDuration;
 
-    private float bountyKillCoolDown;
-    private float punishKillCoolDown;
+    private float dashSpeed = 1f;
+    private Remote<GameOptionOverride>? overrideRemote;
 
-    private IRemote? cooldownOverride;
-
-    private bool Targetarrow
-
-    [UIComponent(UI.Text)]
-    private string ShowTarget() => Color.red.Colorize("Target: ") + Color.white.Colorize(bountyTarget == null ? "None" : bountyTarget.Name);
-
-    [RoleAction(RoleActionType.Attack)]
-    public override bool TryKill(PlayerControl target)
+    [RoleAction(RoleActionType.OnPet)]
+    public void AssumeDash()
     {
-        SendKillCooldown(bountyTarget?.PlayerId == target.PlayerId);
-        bool success = base.TryKill(target);
-        if (success)
-            BountyHunterAcquireTarget();
-        return success;
-    }
-
-    [RoleAction(RoleActionType.FixedUpdate)]
-    private void BountyHunterTargetUpdate()
-    {
-        if (acquireNewTarget.NotReady()) return;
-        BountyHunterAcquireTarget();
-    }
-
-    [RoleAction(RoleActionType.RoundStart)]
-    private void BountyHunterTargetOnRoundStart() => BountyHunterAcquireTarget();
-
-    private void BountyHunterAcquireTarget()
-    {
-        List<PlayerControl> eligiblePlayers = Game.GetAlivePlayers()
-            .Where(p => p.Relationship(MyPlayer) is not Relation.FullAllies)
-            .ToList();
-        if (eligiblePlayers.Count == 0)
-        {
-            bountyTarget = null;
-            return;
-        }
-
-        // Small function to assign a NEW random target unless there's only one eligible target alive
-        PlayerControl newTarget = eligiblePlayers.PopRandom();
-        while (eligiblePlayers.Count > 1 && bountyTarget?.PlayerId == newTarget.PlayerId)
-            newTarget = eligiblePlayers.PopRandom();
-
-        bountyTarget = Game.MatchData.FrozenPlayer(newTarget);
-        acquireNewTarget.Start();
-    }
-
-    private void SendKillCooldown(bool decreased)
-    {
-        float cooldown = decreased ? bountyKillCoolDown : punishKillCoolDown;
-        cooldownOverride?.Delete();
-        cooldownOverride = AddOverride(new GameOptionOverride(Override.KillCooldown, cooldown));
-        SyncOptions();
-    }
-
-        [UIComponent(UI.Indicator)]
-    public string DisplayArrow()
-    {
-        PlayerControl? BountyHunter = Players.FindPlayerById(Target);
-        if (BountyHunter == null) return "";
-            return arrowCache = $"<size=3>{RoleUtils.CalculateArrow(MyPlayer, agent, RoleColor)}</size>";
-        return arrowCache;
+        if (dashCooldown.NotReady() || dashDuration.NotReady()) return;
+        dashDuration.Start();
+        Async.Schedule(() => dashCooldown.Start(), dashDuration.Duration);
+        AdditiveOverride additiveOverride = new(Override.PlayerSpeedMod, playerSpeedIncrease);
+        overrideRemote = Game.MatchData.Roles.AddOverride(MyPlayer.PlayerId, additiveOverride);
+        if (dashDuration = 0) return;
     }
 
     protected override GameOptionBuilder RegisterOptions(GameOptionBuilder optionStream) =>
-        base.RegisterOptions(optionStream)
-            .Color(RoleColor)
+        base.RegisterOptions(optionStream).Color(RoleColor)
             .SubOption(sub => sub
-                .Name("Time Until New Target")
-                .Bind(v => acquireNewTarget.Duration = (float)v)
-                .AddFloatRange(5f, 120, 5, 11)
-                .IOSettings(settings => settings.UnknownValueAction = ADEAnswer.Allow)
+                .KeyName("Dash Movement Speed", Translations.Options.dashSpeed)
+                .AddFloatRange(0.25f, 2.5f, 0.25f, 3)
+                .BindFloat(f => dashSpeed = f)
                 .Build())
-            .SubOption(sub => sub
-                .Name("Kill Cooldown After Killing Target")
-                .Bind(v => bountyKillCoolDown = (float)v)
-                .AddFloatRange(0, 180, 2.5f, 6)
-                .IOSettings(settings => settings.UnknownValueAction = ADEAnswer.Allow)
+            .SubOption(sub => sub.Name("Dash Cooldown")
+                .Bind(v => dashCooldown.Duration = (float)v)
+                .AddFloatRange(2.5f, 120, 2.5f, 5, GeneralOptionTranslations.SecondsSuffix)
                 .Build())
-            .SubOption(sub => sub
-                .Name("Kill Cooldown After Killing Other")
-                .Bind(v => punishKillCoolDown = (float)v)
-                .AddFloatRange(0, 180, 2.5f, 15)
-                .IOSettings(settings => settings.UnknownValueAction = ADEAnswer.Allow)
-                .Build())
-            .SubOption(sub =>  sub
-                .name("Arrow to target")
-                .AddOnOffValues()
-                .BindBool(b => Targetarrow = b)
-                .Build());
+            .SubOption(sub => sub.Name("Dash Duration")
+                .Bind(v => dashDuration.Duration = (float)v)
+                .AddFloatRange(1, 20, 0.25f, 10, GeneralOptionTranslations.SecondsSuffix).Build());
+
+    protected override RoleModifier Modify(RoleModifier roleModifier) =>
+        base.Modify(roleModifier)
+            .VanillaRole(RoleTypes.Impostor)
+            .RoleColor(new Color(1.0f, 0.0f, 0.0f));
 }
