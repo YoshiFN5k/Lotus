eacf52528c3636cb630a8e390b5e10c6b79dbb63
Babydino2-2-3 <116646111+Babydino2-2-3@users.noreply.github.com>
2023-10-30 10:35:44 -0400

    (very baseline) monarch code

---
+++ src/Roles/RoleGroups/Crew/Monarch.cs
@@ -0,0 +1,22 @@
+using Lotus.API;
+using Lotus.Roles.Internals.Attributes;
+using Lotus.Roles.Overrides;
+using Lotus.Roles.RoleGroups.Vanilla;
+using Lotus.Extensions;
+using Lotus.Options;
+using Lotus.Patches.Systems;
+using Lotus.Roles.Internals;
+using UnityEngine;
+using VentLib.Logging;
+using VentLib.Options.Game;
+using VentLib.Utilities;
+using VentLib.Utilities.Optionals;
+
+namespace Lotus.Roles.RoleGroups.Crew;
+
+[Localized("Roles")]
+public class Monarch: Crewmate
+{
+    private bool targetSelected;
+    private 
+}
