7fb5c5f687117e0aa32924046a53608ccd8d1855
Babydino2-2-3 <116646111+Babydino2-2-3@users.noreply.github.com>
2023-11-15 13:05:44 -0500

    begin coding the role for the really cool person

---
+++ src/Roles/RoleGroups/Neutral/Innocent.cs
@@ -0,0 +1,16 @@
+/* TO-DO:
+Literally everything lmao
+rough todo: 
+read through pb and sheri code
+rewrite relevant code
+cause kill to suicide + autoreport instead
+code win condition modification
+~~hate myself~~
+*/
+
+namespace Lotus.Roles.RoleGroups.Neutral;
+
+public class Innocent: NeutralKillingBase
+{
+
+}
--- src/Roles/RoleGroups/NeutralKilling/Sidekick.cs
+++ src/Roles/RoleGroups/NeutralKilling/Sidekick.cs
@@ -3,4 +3,6 @@ namespace Lotus.Roles.RoleGroups.NeutralKilling;
 public class Sidekick : NotImplemented
 {
     // I ASSUME THIS IS JACKAL SIDEKICK
+
+    // FROM DINO: it probably most definitely is lmao
 }
