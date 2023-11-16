5f7255968a2806b874e2f3e9aa70762c5c80703b
Babydino2-2-3 <116646111+Babydino2-2-3@users.noreply.github.com>
2023-11-16 11:41:06 -0500

    miles edgeworth!?!?

--- src/Roles/RoleGroups/Neutral/Innocent.cs
+++ src/Roles/RoleGroups/Neutral/Innocent.cs
@@ -1,10 +1,11 @@
 using Lotus.Interactions;
+using Lotus.Extensions;
 /* TO-DO:
 Literally everything lmao
 rough todo:
 read through pb, sheri/guesser, controlling role, exe, and s cat code
 rewrite relevant code (current)
-cause kill to suicide + autoreport instead
+cause kill to control suicide instead
 code win condition modification
 write in the long list of scripts to borrow
 ~~hate myself~~
@@ -14,9 +15,16 @@ namespace Lotus.Roles.RoleGroups.Neutral;
 
 public class Innocent : NeutralKillingBase
 {
+    public bool EvilAceAttorneyTime;
+    public bool EatedItAll;
+
     [RoleAction(RoleActionType.Attack)]
     public override void TrySelfPortugal(PlayerControl target)
     {
-
+        target.InteractWIth(MyPlayer, LotusInteraction.FatalInteraction.Create(this));
+        EvilAceAttorneyTime = true;
+        return false;
+    
     }
+    [RoleAction(RoleActionType.RoundEnd)]
 }
