213a92c6d0cf738c9a1373900aa133d2d680bc13
YoshiFN5k <142546547+YoshiFN5k@users.noreply.github.com>
2023-12-03 11:45:01 +0000

    Update and rename Prosecutor.cs to Dictator.cs

--- src/Roles/RoleGroups/Crew/Dictator.cs
+++ src/Roles/RoleGroups/Crew/Dictator.cs
@@ -17,74 +17,74 @@ using VentLib.Localization.Attributes;
 using VentLib.Options.Game;
 using VentLib.Utilities.Extensions;
 using VentLib.Utilities.Optionals;
-using static Lotus.Roles.RoleGroups.Crew.Prosecutor.ProsecutorTranslations;
-using static Lotus.Roles.RoleGroups.Crew.Prosecutor.ProsecutorTranslations.ProsecutorOptionTranslations;
+using static Lotus.Roles.RoleGroups.Crew.Dictator.DictatorTranslations;
+using static Lotus.Roles.RoleGroups.Crew.Dictator.DictatorTranslations.DictatorOptionTranslations;
 
 namespace Lotus.Roles.RoleGroups.Crew;
 
-public class Prosecutor: Crewmate
+public class Dictator: Crewmate
 {
-    private static IAccumulativeStatistic<int> _playersEjected = Statistic<int>.CreateAccumulative("Roles.Prosecutor.PlayersEjected", () => PlayersEjectedStat);
+    private static IAccumulativeStatistic<int> _playersEjected = Statistic<int>.CreateAccumulative("Roles.Dictator.PlayersEjected", () => PlayersEjectedStat);
     public override List<Statistic> Statistics() => new() { _playersEjected };
 
     private bool suicideIfVoteCrewmate;
-    private int totalProsecutes;
+    private int totalDictates;
 
-    private int currentProsecutes;
-    private bool showProsecutionsVoteAtEnd;
+    private int currentDictate;
+    private bool showDictatorVoteAtEnd;
 
-    private GameData.PlayerInfo? prosecutedPlayer;
-    private ChatHandler? prosecuteMessage;
+    private GameData.PlayerInfo? exiledPlayer;
+    private ChatHandler? dictateMessage;
     private bool shouldSuicide;
 
     [UIComponent(UI.Counter, ViewMode.Replace, GameState.InMeeting)]
-    private string ProsecuteCounter() => RoleUtils.Counter(currentProsecutes, totalProsecutes, RoleColor);
+    private string DictateCounter() => RoleUtils.Counter(currentDictates, totalDictates, RoleColor);
 
-    protected override void PostSetup() => currentProsecutes = totalProsecutes;
+    protected override void PostSetup() => currentDictates = totalDictates;
 
     [RoleAction(RoleActionType.MyVote)]
-    private void ProsecutorVote(Optional<PlayerControl> target, MeetingDelegate meetingDelegate)
+    private void DictatorVote(Optional<PlayerControl> target, MeetingDelegate meetingDelegate)
     {
         if (!target.Exists()) return;
         PlayerControl player = target.Get();
-        prosecutedPlayer = player.Data;
+        exiledPlayer = player.Data;
 
-        prosecuteMessage = ChatHandler
+        dictateMessage = ChatHandler
             .Of(TranslationUtil.Colorize(DictateMessage.Formatted(player.name, RoleName), RoleColor))
             .Title(t => t.Color(RoleColor).Text(RoleName).Build())
             .LeftAlign();
 
         _playersEjected.Increment(MyPlayer.UniquePlayerId());
-        Game.MatchData.GameHistory.AddEvent(new ProsecutorVoteEvent(MyPlayer, player));
+        Game.MatchData.GameHistory.AddEvent(new DictatorVoteEvent(MyPlayer, player));
 
-        if (--currentProsecutes <= 0) shouldSuicide = false;
-        else if (suicideIfVoteCrewmate && Relationship(player) is not Relation.FullAllies)
+        if (--currentDictates <= 0) shouldSuicide = false;
+        else if (suicideIfVoteNonCrewmate && Relationship(player) is not Relation.FullAllies)
         {
             shouldSuicide = false;
             return;
         }
         else return;
 
-        FinalizeProsecution();
-        meetingDelegate.EndVoting(prosecutedPlayer);
+        FinalizeDictate();
+        meetingDelegate.EndVoting(exiledPlayer);
     }
 
     [RoleAction(RoleActionType.VotingComplete, priority: Priority.High)]
-    private void OverrideProsecutedPlayer(MeetingDelegate meetingDelegate)
+    private void OverrideExiledPlayer(MeetingDelegate meetingDelegate)
     {
-        if (showProsecutionVoteAtEnd && prosecutedPlayer != null && currentProsecutes > 0)
+        if (showDictatorVoteAtEnd && exiledPlayer != null && currentDictates > 0)
         {
-            meetingDelegate.ExiledPlayer = prosecutedPlayer;
-            FinalizeProsecution();
+            meetingDelegate.ExiledPlayer = exiledPlayer;
+            FinalizeDictate();
         }
-        prosecuteMessage = null;
-        prosecutedPlayer = null;
+        dictateMessage = null;
+        dictatePlayer = null;
         shouldSuicide = false;
     }
 
-    private void FinalizeProsecution()
+    private void FinalizeDictate()
     {
-        prosecuteMessage?.Send();
+        dictateMessage?.Send();
         if (!shouldSuicide) return;
 
         ProtectedRpc.CheckMurder(MyPlayer, MyPlayer);
@@ -93,17 +93,17 @@ public class Prosecutor: Crewmate
 
     protected override GameOptionBuilder RegisterOptions(GameOptionBuilder optionStream) =>
         base.RegisterOptions(optionStream)
-            .SubOption(sub => sub.KeyName("Number of Prosecutions", NumberOfDictates)
+            .SubOption(sub => sub.KeyName("Number of Dictates", NumberOfDictates)
                 .AddIntRange(1, 15)
                 .BindInt(i => totalDictates = i)
                 .ShowSubOptionPredicate(i => (int)i > 1)
                 .SubOption(sub2 => sub2
-                    .KeyName("Show Prosecution at End of Meeting", ShowDictatorVoteAtMeetingEnd)
+                    .KeyName("Show Dictator at End of Meeting", ShowDictatorVoteAtMeetingEnd)
                     .BindBool(b => showDictatorVoteAtEnd = b)
                     .AddOnOffValues()
                     .Build())
                 .Build())
-            .SubOption(sub => sub.KeyName("Suicide if Crewmate Prosecuted", TranslationUtil.Colorize(SuicideIfVoteCrewmate, ModConstants.Palette.CrewmateColor))
+            .SubOption(sub => sub.KeyName("Suicide if Crewmate Exiled", TranslationUtil.Colorize(SuicideIfVoteCrewmate, ModConstants.Palette.CrewmateColor))
                 .AddOnOffValues(false)
                 .BindBool(b => suicideIfVoteCrewmate = b)
                 .Build());
@@ -118,32 +118,32 @@ public class Prosecutor: Crewmate
         {
         }
 
-        public override string Message() => TranslationUtil.Colorize(prosecuteMessage.Formatted(Player().name, Target().name), Player().GetRoleColor(), Target().GetRoleColor());
+        public override string Message() => TranslationUtil.Colorize(dictateMessage.Formatted(Player().name, Target().name), Player().GetRoleColor(), Target().GetRoleColor());
     }
 
-    [Localized(nameof(Prosecutor))]
-    internal static class ProsecutorTranslations
+    [Localized(nameof(Dictator))]
+    internal static class DictatorTranslations
     {
         [Localized(nameof(LynchEventMessage))]
         public static string LynchEventMessage = "{0}::0 lynched {1}::1.";
 
-        [Localized(nameof(prosecuteMessage))]
+        [Localized(nameof(dictateMessage))]
         public static string prosecuteMessage = "{0} was voted out by the {1}::0";
 
         [Localized(nameof(PlayersEjectedStat))]
         public static string PlayersEjectedStat = "Players Ejected";
 
         [Localized(ModConstants.Options)]
-        public static class ProsecutorOptionTranslations
+        public static class DictatorOptionTranslations
         {
             [Localized(nameof(NumberOfProsecutions))]
-            public static string NumberOfProsecutions = "Number of Prosecutions";
+            public static string NumberOfDictates = "Number of Dictates";
 
             [Localized(nameof(SuicideIfVoteCrewmate))]
-            public static string SuicideIfVoteCrewmate = "Suicide if Crewmate::0 Executed";
+            public static string SuicideIfVoteNonCrewmate = "Suicide if Non-Crewmate::0 Executed";
 
             [Localized(nameof(ShowProsecutionVoteAtMeetingEnd))]
-            public static string ShowProsecutionVoteAtMeetingEnd = "Show Prosecution at End of Meeting";
+            public static string ShowDictatorVoteAtMeetingEnd = "Show Dictate at End of Meeting";
         }
     }
 }
